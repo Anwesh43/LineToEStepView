fd2aef0c371091094552bd996c85bb3198ca35b1
Anwesh43 <anweshthecool0@gmail.com>
2019-02-18 06:59:51 +0530

    adding libraries and constants

--- linetoestepview/src/main/java/com/anwesh/uiprojects/linetoestepview/LineEToStepView.kt
+++ linetoestepview/src/main/java/com/anwesh/uiprojects/linetoestepview/LineEToStepView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.linetoestepview
  * Created by anweshmishra on 18/02/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val lines : Int = 3
+val nodes : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")
